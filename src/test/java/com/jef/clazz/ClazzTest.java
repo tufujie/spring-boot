fa885dd1993524d3afc4d5d56c724962eb9d67d2
jef.tu@foxmail.com <jef.tu@foxmail.com>
2023-10-30 14:57:32 +0800

    默认使用本地配置

---
+++ src/test/java/com/jef/clazz/ClazzTest.java
@@ -0,0 +1,18 @@
+package com.jef.clazz;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.CompositeIterator;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @author Jef
+ * @date 2023/10/28
+ */
+public class ClazzTest {
+
+    public static void main(String[] args) {
+    }
+
+}
