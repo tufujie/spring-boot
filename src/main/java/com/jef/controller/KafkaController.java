ae112f529216018eacdd4d280cf36bf65ae288ef
tufujie <tufj@hua-cloud.com.cn>
2023-12-26 16:17:58 +0800

    kafka消息顺序性保证

--- src/main/java/com/jef/controller/KafkaController.java
+++ src/main/java/com/jef/controller/KafkaController.java
@@ -1,8 +1,8 @@
 package com.jef.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jef.entity.User;
 import com.jef.service.impl.KafkaUserProducerService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Jef
@@ -25,7 +23,6 @@ public class KafkaController {
     @Value("${kafka.topic.my-topic2}")
     String myTopic2;
     private final KafkaUserProducerService producer;
-    private AtomicInteger atomicLong = new AtomicInteger();
 
     @Autowired
     private KafkaListenerEndpointRegistry registry;
@@ -38,7 +35,7 @@ public class KafkaController {
     public String sendMessageToKafkaTopicByManyPartition(@RequestParam("name") String name) {
         // myTopic有2个分区，当你尝试发送多条消息的时候，你会发现消息会被比较均匀地发送到每个 partion 中。
         for (int i = 0; i < 10; i++) {
-            this.producer.sendMessage(myTopic, new User(atomicLong.addAndGet(1), name));
+            this.producer.sendMessage(myTopic, JSONObject.toJSONString(new User(i, name)));
         }
         return "success";
     }
@@ -47,7 +44,7 @@ public class KafkaController {
     public String sendMessageToKafkaTopicByOnePartition(@RequestParam("name") String name) {
         // 1个topic一个分区，顺序发送和消费
         for (int i = 0; i < 10; i++) {
-            this.producer.sendMessage(myTopic2, new User(atomicLong.addAndGet(1), name));
+            this.producer.sendMessage(myTopic2, JSONObject.toJSONString(new User(i, name)));
         }
         return "success";
     }
--- src/main/java/com/jef/service/impl/KafkaUserConsumerService.java
+++ src/main/java/com/jef/service/impl/KafkaUserConsumerService.java
@@ -1,9 +1,8 @@
 package com.jef.service.impl;
 
-import com.jef.entity.User;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jef.entity.User;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,18 +25,32 @@ public class KafkaUserConsumerService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
 
-    @KafkaListener(topics = {"${kafka.topic.my-topic}"}, groupId = "group1", id = "myListener1", autoStartup = "false")
+    // autoStartup = "false"，是否自动启动,如果是 false,默认不生效,需要手动唤醒。
+    @KafkaListener(topics = {"${kafka.topic.my-topic}"}, groupId = "group1", id = "myListener1")
     public void consumeMessage(ConsumerRecord<String, String> userConsumerRecord) {
         try {
-            User book = objectMapper.readValue(userConsumerRecord.value(), User.class);
-            logger.info("消费者消费topic:{} partition:{}的消息 -> {}", userConsumerRecord.topic(), userConsumerRecord.partition(), book.toString());
+            User user = objectMapper.readValue(userConsumerRecord.value(), User.class);
+            logger.info("消费者1号消费topic:{} partition:{}的消息 -> {}", userConsumerRecord.topic(),
+                    userConsumerRecord.partition(), user.toString());
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
     }
 
-    @KafkaListener(topics = {"${kafka.topic.my-topic2}"}, groupId = "group2", id = "myListener2", autoStartup = "false")
+    @KafkaListener(topics = {"${kafka.topic.my-topic}"}, groupId = "group1", id = "myListener1.1")
+    public void consumeMessage1(ConsumerRecord<String, String> userConsumerRecord) {
+        try {
+            User user = objectMapper.readValue(userConsumerRecord.value(), User.class);
+            logger.info("消费者1号2消费topic:{} partition:{}的消息 -> {}", userConsumerRecord.topic(),
+                    userConsumerRecord.partition(), user.toString());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 一个topic一个分区可以很少的保证有序性
+    @KafkaListener(topics = {"${kafka.topic.my-topic2}"}, groupId = "group2", id = "myListener2")
     public void consumeMessage2(User user) {
-        logger.info("消费者消费{}的消息 -> {}", myTopic2, user.toString());
+        logger.info("消费者2号消费{}的消息 -> {}", myTopic2, user.toString());
     }
 }
